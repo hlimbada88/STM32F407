87346c01c3bbb7e7cb78c9b5946ea323e4456ce1
hlimbada88 <huzaifalimbada@gmail.com>
2020-02-23 13:18:53 +0530

    LED Blink Example.
    Code is used to blink all the led's or perticular led based on the pre-processors directives given in the C/C++ tab in the Option for Target in keil v5.

---
+++ LED_Blinky_Example/MDK-ARM/Pin_PD13/startup_stm32f407xx.d
@@ -0,0 +1,1 @@
+pin_pd13\startup_stm32f407xx.o: startup_stm32f407xx.s
---
+++ LED_Blinky_Example/MDK-ARM/RTE/_Pin_PD13/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Pin_PD13' 
+ * Target:  'Pin_PD13' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
